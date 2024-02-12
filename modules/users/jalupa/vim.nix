4f9b0d10073b0c3771db79c2d762c322520767b6
Jakob Pistohl <gh@jp-bits.de>
2024-02-12 21:08:10 +0100

    Cleanup weird vim config

--- modules/users/jalupa/vim.nix
+++ modules/users/jalupa/vim.nix
@@ -1,38 +1,19 @@
-{pkgs, ...}:
-{
+{ pkgs, ... }: {
   programs.vim = {
     enable = true;
-      #plugins = with pkgs.vimPlugins; [ ];
-      settings = {
-        ignorecase = true;
-      };
-      settings = {
-          #hlsearch = true;
-        };
-        settings = {
-          tabstop = 4;
-        };
-        settings = {
-          #softtabstop = 4;
-        };
-        settings = {
-          expandtab = true;
-        };
-        settings = {
-          shiftwidth = 4;
-        };
-        settings = {
-          #autoindent = true;
-        };
-        settings = {
-          number = true;
-        };
-        settings = {
-          #set encoding = utf8;
-        };
-        settings = {
-          relativenumber = true;
-        };
-      };
-    }
+    #plugins = with pkgs.vimPlugins; [ ];
+    settings = {
+      ignorecase = true;
+      #hlsearch = true;
+      tabstop = 4;
+      #softtabstop = 4;
+      expandtab = true;
+      shiftwidth = 4;
+      #autoindent = true;
+      number = true;
+      #"set encoding" = "utf8";
+      relativenumber = true;
+    };
+  };
+}
 
