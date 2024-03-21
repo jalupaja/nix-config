44f22d10021545e308018021fbd5207502029d6a
Jakob Pistohl <gh@jp-bits.de>
2024-03-21 17:49:43 +0100

    Fix startup script

--- modules/users/jalupa/scripts/startup.nix
+++ modules/users/jalupa/scripts/startup.nix
@@ -3,10 +3,10 @@
   startup = pkgs.pkgs.writeShellScript "startup" ''
     ${pkgs.waybar}/bin/waybar &
     ${pkgs.swww}/bin/swww init
+    $(cat ~/.wallpapercall)
 
-    ${pkgs.syncthing}/bin/syncthing &
+    #${pkgs.syncthing}/bin/syncthing &
 
     # start wallpaper using my wallpaper script
-    $(cat ~/.wallpapercall)
   '';
 }
