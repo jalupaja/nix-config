8c62ea496fb45111a9b53d92289644ab543131eb
Jakob Pistohl <gh@jp-bits.de>
2024-03-27 12:45:01 +0100

    Update nixvim.nix formatting (should fix this in config some day)

--- modules/users/jalupa/nixvim.nix
+++ modules/users/jalupa/nixvim.nix
@@ -79,10 +79,10 @@ programs.nixvim = {
           enable = false;
         };
 
-	nvim-tree = {
-	    enable = true;
-	    #disableNetrw = true;
-	};
+				nvim-tree = {
+					enable = true;
+					#disableNetrw = true;
+				};
 
         # TODO
         #clipboard-image.enable = true;
@@ -97,23 +97,23 @@ programs.nixvim = {
           diffBinaries = true;
         };
 
-        lsp = {
-          enable = true;
-          servers = {
-            # Programming languages
-            clangd.enable = true; # C/C++
-            pyright.enable = true; # python
-	    java-language-server.enable = false; # java
-	    rust-analyzer = { # rust
-		enable = true;
-		installRustc = true;
-		installCargo = true;
-	    };
+				lsp = {
+					enable = true;
+					servers = {
+						# Programming languages
+						clangd.enable = true; # C/C++
+							pyright.enable = true; # python
+							java-language-server.enable = false; # java
+							rust-analyzer = { # rust
+								enable = true;
+								installRustc = true;
+								installCargo = true;
+							};
 
-            # script languages
-            bashls.enable = true; # bash
-            cmake.enable = true; # cmake
-            nixd.enable = true; # nix files
+						# script languages
+						bashls.enable = true; # bash
+							cmake.enable = true; # cmake
+							nixd.enable = true; # nix files
 
             #web stuff
             html.enable = true; # html
@@ -158,21 +158,21 @@ programs.nixvim = {
         lsp-format = {
           enable = true;
           lspServersToEnable = "all";
-        };
-        #lsp-lines.enable = true;
-	# java lsp
-	nvim-jdtls = {
-	    enable = true;
-	    cmd = [
-		    # Eclipse language server
-		    "${pkgs.jdt-language-server}/bin/jdtls"
-		    "-noverify"
-		    "-Xms1G"
-		    "-jar"
-		    "-data" ".cache/jdtls/workspace"
-		    "-configuration" ".cache/jdtls/config"
-	    ];
-	};
+				};
+				#lsp-lines.enable = true;
+				# java lsp
+				nvim-jdtls = {
+					enable = true;
+					cmd = [
+						# Eclipse language server
+						"${pkgs.jdt-language-server}/bin/jdtls"
+							"-noverify"
+							"-Xms1G"
+							"-jar"
+							"-data" ".cache/jdtls/workspace"
+							"-configuration" ".cache/jdtls/config"
+					];
+				};
 
         # more LaTex features
         vimtex = {
@@ -211,239 +211,238 @@ programs.nixvim = {
           };
         };
 
-        # TODO doesn't work (never tested in css files...)
-        nvim-colorizer = {
-          enable = true;
-        };
+				# TODO doesn't work (never tested in css files...)
+				nvim-colorizer = {
+					enable = true;
+				};
 
-	lspkind = {
-	    enable = true;
+				lspkind = {
+					enable = true;
 
-	    cmp = {
-		enable = true;
-		menu = {
-		    nvim_lsp = "[LSP]";
-		    nvim_lua = "[api]";
-		    path = "[path]";
-		    luasnip = "[snip]";
-		    buffer = "[buffer]";
-		    neorg = "[neorg]";
-		    cmp_tabby = "[Tabby]";
+					cmp = {
+						enable = true;
+						menu = {
+							nvim_lsp = "[LSP]";
+							nvim_lua = "[api]";
+							path = "[path]";
+							luasnip = "[snip]";
+							buffer = "[buffer]";
+							neorg = "[neorg]";
+							cmp_tabby = "[Tabby]";
+						};
+					};
+				};
+
+				cmp = {
+					enable = true;
+
+					settings = {
+						snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+						mapping = {
+							"<C-d>" = "cmp.mapping.scroll_docs(-4)";
+							"<C-f>" = "cmp.mapping.scroll_docs(4)";
+							"<C-Space>" = "cmp.mapping.complete()";
+							"<C-e>" = "cmp.mapping.close()";
+							"<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+							"<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+							"<CR>" = "cmp.mapping.confirm({ select = true })";
+						};
+
+						sources = [
+						{name = "path";}
+						{name = "nvim_lsp";}
+						{name = "cmp_tabby";}
+						{name = "luasnip";}
+						{
+							# Words from other open buffers can also be suggested.
+							name = "buffer";
+							option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+						}
+						{name = "neorg";}
+						];
+					};
+				};
+
+				cmp-nvim-lsp.enable = true;
+				cmp_luasnip.enable = true;
+
+				luasnip.enable = true;
 		};
-	    };
-	};
 
-	cmp = {
-	    enable = true;
+		keymaps = [
+		# Those are somewhat doom-emacs keybinds...
+		{
+			# Escape out of terminal
+			mode = "t";
+			key = "<Esc><Esc>";
+			action = "<C-\\><C-n>";
+		}
+		{
+			# current project
+			mode = "n";
+			key = "<leader>fr";
+			action = "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>";
+		}
+		{
+			# file finder
+			mode = "n";
+			key = "<leader>.";
+			action = "<cmd>lua require'telescope.builtin'.find_files{}<CR>";
+		}
+		{
+			# Neogit
+			mode = "n";
+			key = "<leader>gg";
+			action = "<cmd>Neogit<CR>";
+		}
+		{
+			# Commenting
+			key = "<leader>c";
+			action = "<cmd>Commentary<CR>";
+		}
+		{
+			# rename variable in Buffer
+			key = "<leader>r";
+			action = "<cmd>Lspsaga rename<CR>";
+		}
+		{
+			# File Tree
+			mode = "n";
+			key = "<leader>d";
+			action = "<cmd>NvimTreeToggle<CR>";
+		}
+		{
+			# rename variable in Project
+			key = "<leader>pr";
+			action = "<cmd>Lspsaga rename ++project<CR>";
+		}
+		{
+			# floating terminal
+			mode = "n";
+			key = "<leader>t";
+			action = "<cmd>Lspsaga term_toggle<CR>";
+		}
 
-	    settings = {
-		snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
-		mapping = {
-		    "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-		    "<C-f>" = "cmp.mapping.scroll_docs(4)";
-		    "<C-Space>" = "cmp.mapping.complete()";
-		    "<C-e>" = "cmp.mapping.close()";
-		    "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-		    "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
-		    "<CR>" = "cmp.mapping.confirm({ select = true })";
-		};
+		# Lsp keybindings
+		{
+			# hover definition
+			mode = "n";
+			key = "<leader>gh";
+			action = "<cmd>Lspsaga hover_doc<CR>";
+		}
+		{
+			# definition
+			mode = "n";
+			key = "<leader>gd";
+			action = "<cmd>Lspsaga peek_definition<CR>";
+		}
+		{
+			# type definition
+			mode = "n";
+			key = "<leader>gt";
+			action = "<cmd>Lspsaga peek_type_definition<CR>";
+		}
+		{
+			# type definition
+			mode = "n";
+			key = "<leader>f";
+			action = "<cmd>Lspsaga finder<CR>";
+		}
 
-		sources = [
-		    {name = "path";}
-		    {name = "nvim_lsp";}
-		    {name = "cmp_tabby";}
-		    {name = "luasnip";}
-		    {
-			name = "buffer";
-			# Words from other open buffers can also be suggested.
-			option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
-		    }
-		    {name = "neorg";}
+		# remap Control to leader key
+		## movement
+		{
+			mode = "n";
+			key = "<leader>ww";
+			action = "<C-w>w";
+		}
+		{
+			mode = "n";
+			key = "<leader>wh";
+			action = "<C-w>h";
+		}
+		{
+			mode = "n";
+			key = "<leader>w<Left>";
+			action = "<C-w>h";
+		}
+		{
+			mode = "n";
+			key = "<leader>wj";
+			action = "<C-w>j";
+		}
+		{
+			mode = "n";
+			key = "<leader>w<Down>";
+			action = "<C-w>j";
+		}
+		{
+			mode = "n";
+			key = "<leader>wk";
+			action = "<C-w>k";
+		}
+		{
+			mode = "n";
+			key = "<leader>w<Up>";
+			action = "<C-w>k";
+		}
+		{
+			mode = "n";
+			key = "<leader>wl";
+			action = "<C-w>l";
+		}
+		{
+			mode = "n";
+			key = "<leader>w<Right>";
+			action = "<C-w>l";
+		}
+
+		## split
+		{
+			mode = "n";
+			key = "<leader>ws";
+			action = "<C-w>s";
+		}
+		{
+			mode = "n";
+			key = "<leader>wv";
+			action = "<C-w>v";
+		}
+		{
+			mode = "n";
+			key = "<leader>wq";
+			action = "<cmd>q<CR>";
+		}
+
+		## buffer
+		{
+			mode = "n";
+			key = "<leader>bp";
+			action = "<cmd>bp<CR>";
+		}
+		{
+			mode = "n";
+			key = "<leader>bn";
+			action = "<cmd>bn<CR>";
+		}
+		{
+			mode = "n";
+			key = "<leader>bd";
+			action = "<cmd>bd<CR>";
+		}
+
+		## access global clipboard
+		{
+			mode = "";
+			key = "<leader>y";
+			action = ''"+y'';
+		}
+		{
+			mode = "";
+			key = "<leader>p";
+			action = ''"+p'';
+		}
 		];
-	    };
-	};
-
-	cmp-nvim-lsp.enable = true;
-	cmp_luasnip.enable = true;
-
-        luasnip.enable = true;
-        #cmp-nvim-ultisnips.enable = true;
-      };
-
-      keymaps = [
-    # Those are somewhat doom-emacs keybinds...
-    {
-        # Escape out of terminal
-        mode = "t";
-        key = "<Esc><Esc>";
-        action = "<C-\\><C-n>";
-      }
-      {
-        # current project
-        mode = "n";
-        key = "<leader>fr";
-        action = "<cmd>lua require'telescope.builtin'.oldfiles{}<CR>";
-      }
-      {
-        # file finder
-        mode = "n";
-        key = "<leader>.";
-        action = "<cmd>lua require'telescope.builtin'.find_files{}<CR>";
-      }
-      {
-        # Neogit
-        mode = "n";
-        key = "<leader>gg";
-        action = "<cmd>Neogit<CR>";
-      }
-      {
-        # Commenting
-        key = "<leader>c";
-        action = "<cmd>Commentary<CR>";
-      }
-      {
-        # rename variable in Buffer
-        key = "<leader>r";
-        action = "<cmd>Lspsaga rename<CR>";
-      }
-      {
-        # File Tree
-	mode = "n";
-        key = "<leader>d";
-        action = "<cmd>NvimTreeToggle<CR>";
-      }
-      {
-        # rename variable in Project
-        key = "<leader>pr";
-        action = "<cmd>Lspsaga rename ++project<CR>";
-      }
-      {
-        # floating terminal
-        mode = "n";
-        key = "<leader>t";
-        action = "<cmd>Lspsaga term_toggle<CR>";
-      }
-
-    # Lsp keybindings
-    {
-        # hover definition
-        mode = "n";
-        key = "<leader>gh";
-        action = "<cmd>Lspsaga hover_doc<CR>";
-      }
-      {
-        # definition
-        mode = "n";
-        key = "<leader>gd";
-        action = "<cmd>Lspsaga peek_definition<CR>";
-      }
-      {
-        # type definition
-        mode = "n";
-        key = "<leader>gt";
-        action = "<cmd>Lspsaga peek_type_definition<CR>";
-      }
-      {
-        # type definition
-        mode = "n";
-        key = "<leader>f";
-        action = "<cmd>Lspsaga finder<CR>";
-      }
-
-    # remap Control to leader key
-    ## movement
-    {
-      mode = "n";
-      key = "<leader>ww";
-      action = "<C-w>w";
-    }
-    {
-      mode = "n";
-      key = "<leader>wh";
-      action = "<C-w>h";
-    }
-    {
-      mode = "n";
-      key = "<leader>w<Left>";
-      action = "<C-w>h";
-    }
-    {
-      mode = "n";
-      key = "<leader>wj";
-      action = "<C-w>j";
-    }
-    {
-      mode = "n";
-      key = "<leader>w<Down>";
-      action = "<C-w>j";
-    }
-    {
-      mode = "n";
-      key = "<leader>wk";
-      action = "<C-w>k";
-    }
-    {
-      mode = "n";
-      key = "<leader>w<Up>";
-      action = "<C-w>k";
-    }
-    {
-      mode = "n";
-      key = "<leader>wl";
-      action = "<C-w>l";
-    }
-    {
-      mode = "n";
-      key = "<leader>w<Right>";
-      action = "<C-w>l";
-    }
-
-    ## split
-    {
-      mode = "n";
-      key = "<leader>ws";
-      action = "<C-w>s";
-    }
-    {
-      mode = "n";
-      key = "<leader>wv";
-      action = "<C-w>v";
-    }
-    {
-      mode = "n";
-      key = "<leader>wq";
-      action = "<cmd>q<CR>";
-    }
-
-    ## buffer
-    {
-      mode = "n";
-      key = "<leader>bp";
-      action = "<cmd>bp<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>bn";
-      action = "<cmd>bn<CR>";
-    }
-    {
-      mode = "n";
-      key = "<leader>bd";
-      action = "<cmd>bd<CR>";
-    }
-
-    ## access global clipboard
-    {
-      mode = "";
-      key = "<leader>y";
-      action = ''"+y'';
-    }
-    {
-      mode = "";
-      key = "<leader>p";
-      action = ''"+p'';
-    }
-  ];
 
   autoCmd = [
   # from https://github.com/GaetanLepage/nix-config/blob/master/home/modules/tui/neovim/autocommands.nix
