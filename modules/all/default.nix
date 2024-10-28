d2cd827c20d277195a2732d6ff9544e5453b45a8
Jakob Pistohl <gh@jp-bits.de>
2024-10-28 22:10:38 +0100

    update kernel

--- modules/all/default.nix
+++ modules/all/default.nix
@@ -17,6 +17,9 @@
       settings.experimental-features = [ "nix-command" "flakes" ];
   };
 
+	# Kernel
+	boot.kernelPackages = pkgs.linuxPackages_latest;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
