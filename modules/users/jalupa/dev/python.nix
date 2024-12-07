49bc5756a4359e3a867144791a8c63878822cff5
Jakob Pistohl <gh@jp-bits.de>
2024-12-07 11:13:36 +0100

    Add ruff to python dev packages

--- modules/users/jalupa/dev/python.nix
+++ modules/users/jalupa/dev/python.nix
@@ -1,7 +1,8 @@
 { pkgs, ... }:
 {
-  home.packages = [
-    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+  home.packages = with pkgs; [
+		ruff
+    (python3.withPackages (python-pkgs: with python-pkgs; [
       requests
       virtualenv
 
