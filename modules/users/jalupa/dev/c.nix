456288fe803f1783f215352a9e98675c356a220a
Jakob Pistohl <gh@jp-bits.de>
2024-12-31 13:11:46 +0100

    install gcc

--- modules/users/jalupa/dev/c.nix
+++ modules/users/jalupa/dev/c.nix
@@ -1,15 +1,15 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    #libgcc
-    #gdb
-		# gcc
+    libgcc
+    gdb
+		gcc
     cmake
     gnumake
 
     # Embedded C development
     # pico-sdk # RP2040 SDK (this will install it into the nix-store and idk how to reliably get its path)
-    gcc-arm-embedded # collides with libgcc and gdb
+    # gcc-arm-embedded # collides with libgcc and gdb
     picotool # RP2040 flashing programm
     minicom # communication with (in my case) the pi pico
   ];
