bae376393b3096756fb07c9bd73fecbddc6ca6d8
Jakob Pistohl <gh@jp-bits.de>
2025-08-10 18:55:48 +0200

    Add mousepad on/off to disable dmenu script

--- modules/users/jalupa/scripts/disable.nix
+++ modules/users/jalupa/scripts/disable.nix
@@ -8,12 +8,18 @@
     options="touch on\ntouch off\nmousepad on\nmousepad off"
 
     # find device names using hyprctl devices
-    case $(echo -e $options | $DMENU "Select device") in
+    case $(echo -e "$options" | $DMENU "Select device") in
       "touch on")
           hyprctl keyword "device[wacom-hid-523a-finger]:enabled" true
         ;;
       "touch off")
           hyprctl keyword "device[wacom-hid-523a-finger]:enabled" false
+        ;;
+      "mousepad on")
+          hyprctl keyword "device[msft0001:01-06cb:ce37-touchpad]:enabled" true
+        ;;
+      "mousepad off")
+          hyprctl keyword "device[msft0001:01-06cb:ce37-touchpad]:enabled" false
         ;;
     esac
 			'';
